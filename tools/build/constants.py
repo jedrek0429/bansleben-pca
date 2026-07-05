ad6adcf9b3ef4c8d522a89663d21c2bddc45788d
Andrzej Bansleben <a.bansleben0429@gmail.com>
2026-07-05 17:17:10 +0200

    Refactor build constants

---
+++ tools/build/constants.py
@@ -0,0 +1,15 @@
+"""Constants for the PCA static site builder."""
+
+DEFAULT_LANGS = ["en", "fr", "hr"]
+CARD_IMAGE_WIDTHS = (360, 300)
+CARD_IMAGE_VARIANT_SUFFIX = "-300x200"
+SOCIAL_IMAGE_WIDTH = 1200
+SOCIAL_IMAGE_HEIGHT = 630
+APPLE_TOUCH_ICON_SIZE = 180
+WEBP_QUALITY = "80"
+TEMPLATE_RECURSION_LIMIT = 12
+DEFAULT_THEME_COLOR = "#317041"
+DEFAULT_SITE_SHORT_NAME = "PCA"
+DEFAULT_SITE_NAME = "Poland Child Abduction"
+DEFAULT_LEGAL_PRACTICE_URL = "https://bansleben.pl/"
+DEFAULT_MENU_KEYS = ["introduction", "whom_to_contact", "jurisprudence", "contact", "who_we_are"]
