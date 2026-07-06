59bc04fbef521c9954b1512817a346dd8b0aa174
Andrzej Bansleben <a.bansleben0429@gmail.com>
2026-07-06 12:47:05 +0200

    Use runner CLI with locale autofix

--- tools/build.py
+++ tools/build.py
@@ -9,7 +9,7 @@ BUILD_PACKAGE_DIR = Path(__file__).resolve().with_name("build")
 if str(BUILD_PACKAGE_DIR) not in sys.path:
     sys.path.insert(0, str(BUILD_PACKAGE_DIR))
 
-from app import main
+from runner import main
 
 
 if __name__ == "__main__":
